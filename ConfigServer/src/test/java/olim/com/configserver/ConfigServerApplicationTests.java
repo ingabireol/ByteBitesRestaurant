bd31f00b167331eded8510c7ecf2c99cb8628189
ingabireol <ingabire069@gmail.com>
2025-07-08 12:30:34 +0200

    feat: initialized config server

---
+++ ConfigServer/src/test/java/olim/com/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package olim.com.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
